50ff0ccc7393bdae6cd9473060bd6615d1cda06b
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-20 00:24:45 -0300

    feat: Create purchaseAssets route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import 'express-async-errors';
+import purchaseAssets from './routes/investmentsRoute';
 
 const app = express();
 
 app.use(express.json());
+app.use('/', purchaseAssets)
 
 export default app;
