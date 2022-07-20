6152f5ba0baa6ab9167d80227627919da063370f
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-20 20:58:12 -0300

    feat: error middleware

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 require('express-async-errors');
 const purchaseAssets = require('./routes/investmentsRoute');
 const accountRoute = require('./routes/accountRoute');
+const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 
 app.use(express.json());
 app.use('/', purchaseAssets);
 app.use('/', accountRoute);
-
+app.use(errorMiddleware);
 module.exports = app;
