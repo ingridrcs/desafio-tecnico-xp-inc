b84826aa1ef066e64d679d42ba7aa79ff536115f
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-21 12:34:24 -0300

    refactor: changing saldo to withdraw

--- src/middlewares/withdrawMiddleware.js
+++ src/middlewares/withdrawMiddleware.js
@@ -1,11 +1,12 @@
 const withdrawMiddle = (req, res, next) => {
-  const { codCliente, saldo } = req.body;
+  const { codCliente, withdraw } = req.body;
   if(!codCliente) {
     return res.status(400).json({message:"Cliente não encontrado"});
   }
-  if(saldo <= 0) {
+  if(withdraw <= 0) {
     return res.status(400).json({message:" Saque não autorizado"});
   }
 next();
 }
+
 module.exports = withdrawMiddle;
