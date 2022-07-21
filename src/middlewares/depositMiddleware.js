fb6130f91b51f8ce0e7d615906178eb7b7fff44c
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-21 13:32:42 -0300

    refactor: changing phrase to english

--- src/middlewares/depositMiddleware.js
+++ src/middlewares/depositMiddleware.js
@@ -1,10 +1,10 @@
 const depositMiddle = (req, res, next) => {
   const { codCliente, deposit } = req.body;
   if(!codCliente) {
-    return res.status(400).json({message:"Cliente não encontrado"});
+    return res.status(400).json({message:"Client not found"});
   }
   if(deposit <= 0) {
-    return res.status(400).json({message:" Depósito não autorizado"});
+    return res.status(400).json({message:" Deposit not autorized"});
   }
 next();
 }
--- src/middlewares/withdrawMiddleware.js
+++ src/middlewares/withdrawMiddleware.js
@@ -1,10 +1,10 @@
 const withdrawMiddle = (req, res, next) => {
   const { codCliente, withdraw } = req.body;
   if(!codCliente) {
-    return res.status(400).json({message:"Cliente não encontrado"});
+    return res.status(400).json({message:"Client not found"});
   }
   if(withdraw <= 0) {
-    return res.status(400).json({message:" Saque não autorizado"});
+    return res.status(400).json({message:" Withdraw not autorized"});
   }
 next();
 }
