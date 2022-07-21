4d7de933c429f52aa1d7e140418b425ac6ff736a
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-21 12:35:39 -0300

    refactor: update fuction/remove commented code

--- src/models/accountModel.js
+++ src/models/accountModel.js
@@ -1,21 +1,14 @@
-// import { ResultSetHeader } from 'mysql2';
-// import { ResultSetHeader } from 'mysql2';
 const connection = require('../db/connection');
-//import { IAccount } from '../interfaces/clientAccount';
-
-// const addAccount = async (account:IAccount):Promise<ResultSetHeader> => {
-//   const [row] = await connection.execute<ResultSetHeader>('INSERT INTO TechBank.Accounts (saldo) VALUES (?)', [account.saldo]);
-//   return row;
-// };
 
 const getCodCliente = async (codCliente) => {
-const [client] = await connection.execute('SELECT * FROM TechBank.Accounts WHERE codCliente = ?;', [codCliente]);
-console.log(client);
-return client;
-}
-const update = async (codCliente, saldo) => {
-  const [client] = await connection.execute(
-    'UPDATE TechBank.Accounts SET saldo = ? WHERE codCliente = ?', [saldo, codCliente]);
+  const [client] = await connection.execute('SELECT * FROM TechBank.Accounts WHERE codCliente = ?;', [codCliente]);
   return client;
 }
+
+const update = async (codCliente, saldoFinal) => {
+  const [withdraw] = await connection.execute(
+    'UPDATE TechBank.Accounts SET saldo = ? WHERE codCliente = ?', [saldoFinal, codCliente]);
+  return withdraw;
+}
+
 module.exports = { getCodCliente, update };
