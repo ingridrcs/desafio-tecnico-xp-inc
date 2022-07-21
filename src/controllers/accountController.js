72fd6de6d9f96111a8079ff54d8a44716dd2efc1
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-21 12:26:01 -0300

    feat/refactor: withdraw controller

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,28 +1,21 @@
 const service = require('../services/accountService');
 
-
-// const addAccount = async (req: Request, res: Response):Promise<Response> => {
-//   const account = await addAccountService(req.body);
-//   // const token = generateJWTToken(user);
-//   // if (token) {
-//     return res.status(201).json( account );
-//   // }
-//   // return res.status(409).json({ message: 'User already exists' });
-// };
-
 const getCodCliente = async (req, res) => {
-  const codCliente = Number(req.params.codCliente);
-  console.log(codCliente);
+  const codCliente = req.params.codCliente;
   const cliente = await service.getCliente(codCliente);
+  
   return res.status(201).json({ cliente });
 }
 
 const addWithdraw = async (req, res) => {
-  const withdraw = await service.addWithdraw(req.body);
-  console.log('controller', withdraw);
-  if (withdraw === null) {
+  const { codCliente, withdraw } = req.body;
+  const withdrawCliente = await service.addWithdraw(codCliente, withdraw);
+
+  if (withdrawCliente === null) {
     return res.status(404).json({ message: "Insuficient funds!" });
   }
-  return res.status(201).json({ withdraw });
+
+  return res.status(201).json(withdrawCliente);
 }
+
 module.exports = { getCodCliente, addWithdraw };
