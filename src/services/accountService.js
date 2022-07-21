a9aee7db3ad41bc1a6c7ec7584db360cb48772bc
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-21 12:37:11 -0300

    refactor:remove comented code/addWithdraw function

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -1,28 +1,27 @@
 const model = require('../models/accountModel');
 
-
-// const addAccountService = async (account:IAccount):Promise<IAccount> => {
-//   const newUser = await addAccount(account);
-//   console.log('service', newUser);
-//   const { insertId } = newUser;
-//   return { codCliente: insertId, ...account };
-// };
-
 const getCliente = async (codCliente) => {
   const cliente = await model.getCodCliente(codCliente);
   return cliente;
 }
 
 const addWithdraw = async (codCliente, withdraw) => {
-  const getCliente = await model.getCodCliente(codCliente);
+  const [getCliente] = await model.getCodCliente(codCliente);
   const { saldo } = getCliente;
 
   if (withdraw > saldo) {
     return null;
   }
-  const saldofinal = saldo - withdraw;
-  const update = await model.update(codCliente, saldofinal);
-  console.log(update);
-  return update;
+
+  const saldoFinal = saldo - withdraw;
+  await model.update(codCliente, saldoFinal);
+
+  const finalWithdraw = {
+    codCliente,
+    saldoFinal,
+  }
+
+  return finalWithdraw;
 }
+
 module.exports = { getCliente, addWithdraw };
