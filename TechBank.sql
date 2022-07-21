8df7198bde0171554d43f34b3a45a4aef240a0c9
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-21 15:12:57 -0300

    feat:create assets's  table on SQL

--- TechBank.sql
+++ TechBank.sql
@@ -1,25 +1,33 @@
 DROP SCHEMA IF EXISTS TechBank;
+
 CREATE SCHEMA IF NOT EXISTS TechBank;
 
-	CREATE TABLE TechBank.Investments (
-    codCliente INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    codAtivo INTEGER,
-    qtdeAtivo INTEGER
-    ) ENGINE=INNODB;
+CREATE TABLE
+    TechBank.Investments (
+        codCliente INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+        codAtivo INTEGER,
+        qtdeAtivo INTEGER
+    ) ENGINE = INNODB;
 
-  INSERT INTO TechBank.Investments (codAtivo, qtdeAtivo)
-VALUES 
-(100, 100),
-(101, 100),
-(102, 100);
+INSERT INTO
+    TechBank.Investments (codAtivo, qtdeAtivo)
+VALUES (100, 100), (101, 100), (102, 100);
 
-	CREATE TABLE TechBank.Accounts (
-    codCliente INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    saldo DECIMAL
-    ) ENGINE=INNODB;
+CREATE TABLE
+    TechBank.Accounts (
+        codCliente INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+        saldo INTEGER
+    ) ENGINE = INNODB;
 
-  INSERT INTO TechBank.Accounts (saldo)
-VALUES 
-(100),
-(101),
-(10);
+INSERT INTO TechBank.Accounts (saldo) VALUES (100), (101), (10);
+
+CREATE TABLE
+    TechBank.Assets (
+        CodAtivo INTEGER PRIMARY KEY NOT NULL,
+        QtAtivo INTEGER,
+        Valor DECIMAL(5, 2)
+    ) ENGINE = INNODB;
+
+INSERT INTO
+    TechBank.Assets (CodAtivo, QtAtivo, Valor)
+VALUES (111, 100, 35.00), (222, 100, 35.00), (333, 100, 35.00);
