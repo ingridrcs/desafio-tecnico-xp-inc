b52a46da46afe2c53ade1d9f75e390f5e5a62fdd
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-22 02:55:27 -0300

    feat: finishing service of purchase

--- src/services/investmentsService.js
+++ src/services/investmentsService.js
@@ -5,18 +5,37 @@ const addPurchase = async (codCliente, codAtivo, quantity) => {
    const [getTableAssets] = await modelAssets.getByAssets(codAtivo);
    const { QtdeAtivo, Valor } = getTableAssets;
    const qtdeAtivo = (QtdeAtivo - quantity);
-   
-   if(qtdeAtivo < 0) {
+
+   if (qtdeAtivo < 0) {
       return null;
    }
-   await modelPurchase(codAtivo, qtdeAtivo, Valor);
+
+   await modelPurchase.addPurchaseAssets(codAtivo, qtdeAtivo, Valor);
+
+   const [getClient] = await modelAssets.getAllByClients(codCliente);
+   const [getAsset] = await getClient.filter((asset) => codAtivo === asset.CodAtivo)
+      .map((asset) => {
+
+         const totalAssets = asset.QtdeAtivo + quantity;
+         const totalValue = Number((quantity * Valor)) + Number(asset.Valor);
+
+         return {
+            cliente: asset.CodCliente,
+            ativo: asset.CodAtivo,
+            quantidade: totalAssets,
+            valor: totalValue,
+         }
+      });
+
+   const { cliente, ativo, quantidade, valor } = getAsset;
+   await modelPurchase.updateAssetsClients(cliente, ativo, quantidade, valor);
 
    const purchase = {
       codCliente,
       codAtivo,
       qtdeAtivo,
    }
-   
+
    return purchase;
 };
 
