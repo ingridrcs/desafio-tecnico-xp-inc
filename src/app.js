dd5b63909c608608659210b4596c5aac656fe1dd
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-22 01:10:51 -0300

    refactor:changing names

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('express-async-errors');
-const purchaseAssetsRoute = require('./routes/investmentsRoute');
+const investmentsRoute = require('./routes/investmentsRoute');
 const accountRoute = require('./routes/accountRoute');
 const errorMiddleware = require('./middlewares/error');
 const assetsRoute = require('./routes/assetsRoute');
@@ -8,7 +8,7 @@ const assetsRoute = require('./routes/assetsRoute');
 const app = express();
 
 app.use(express.json());
-app.use('/', purchaseAssetsRoute);
+app.use('/', investmentsRoute);
 app.use('/', accountRoute);
 app.use('/', assetsRoute);
 app.use(errorMiddleware);
