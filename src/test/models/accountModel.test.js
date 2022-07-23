82a845aa6a45a6e4c6ba940e041096127b3618b5
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-23 16:46:59 -0300

    test:accountModel

---
+++ src/test/models/accountModel.test.js
@@ -0,0 +1,34 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const accountModel = require("../../models/accountModel");
+const connection = require("../../db/connection");
+
+
+describe('Testar a camada Account Model', () => {
+  describe('Ver se o endpoint GET/account/:codCliente retorna os dados do cliente', () => {
+    const insertId = 1;
+
+    before(async () => {
+
+      const client = [{
+        "codCliente": 1,
+        "saldo": 1000
+      }];
+      sinon.stub(connection, 'execute').resolves(client);
+    });
+
+    after(async () => {
+      connection.execute.restore();
+    });
+    it('Retorna um objeto', async () => {
+      const response = await accountModel.getCodCliente(insertId);
+      expect(response).to.be.an("object");
+    });
+    it('retorna "codCliente" e "saldo" ', async () => {
+      const response = await accountModel.getCodCliente(insertId);
+      expect(response).to.have.property("codCliente");
+      expect(response).to.have.property("saldo");
+    })
+  })
+  
+});
