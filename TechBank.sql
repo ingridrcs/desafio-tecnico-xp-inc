3c1daa80575087122f25241416c5559b4a4e89a7
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-23 02:38:23 -0300

    feat: create user table

--- TechBank.sql
+++ TechBank.sql
@@ -16,21 +16,21 @@ VALUES (100, 100), (101, 100), (102, 100);
 CREATE TABLE
     TechBank.Accounts (
         codCliente INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-        saldo INTEGER
+        saldo INTEGER 
     ) ENGINE = INNODB;
 
-INSERT INTO TechBank.Accounts (saldo) VALUES (100), (101), (10);
+INSERT INTO TechBank.Accounts (saldo) VALUES (1000), (5000), (14000), (500);
 
 CREATE TABLE
     TechBank.Assets (
         CodAtivo INTEGER PRIMARY KEY NOT NULL,
-        QtdeAtivo INTEGER,
-        Valor DECIMAL(5, 2)
+        QtdeAtivo INTEGER NOT NULL,
+        Valor DECIMAL(6, 2) NOT NULL
     ) ENGINE = INNODB;
 
 INSERT INTO
     TechBank.Assets (CodAtivo, QtdeAtivo, Valor)
-VALUES (111, 100, 35.00), (222, 100, 35.00), (333, 100, 35.00), (444, 100, 35.00);
+VALUES (111, 100, 30.00), (222, 100, 35.00), (333, 100, 15.00), (444, 50, 40.00), (555, 50, 27.00);
 
 CREATE TABLE
     TechBank.AssetsClient (
@@ -50,4 +50,17 @@ INSERT INTO
         QtdeAtivo,
         Valor
     )
-VALUES (1, 111, 100, 350.00), (1, 222, 100, 350.00), (1, 333, 100, 350.00);
+VALUES (1, 111, 100, 3000.00), (1, 222, 100, 3500.00), (2, 333, 100, 1500.00), (2, 111, 10, 300.00 ), (3, 444, 50, 2000.00), (4, 222, 10, 350.00);
+
+CREATE TABLE
+    TechBank.User (
+        id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        senha VARCHAR(50) NOT NULL,
+        codCliente INTEGER ,
+        FOREIGN KEY (codCliente) REFERENCES Accounts (codCliente) ON DELETE CASCADE
+    ) ENGINE = INNODB;
+
+INSERT INTO
+    TechBank.User (email, senha, codCliente)
+VALUES ("ingrid@gmail.com", 125587, 1), ("artur@gmail.com", 654321, 2), ("luciana@gmail.com", 457895, 3), ("mario@gmail.com", 156489, 4);
