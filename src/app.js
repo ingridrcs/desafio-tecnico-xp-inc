19d4dd80a58b9e7ba5e93213f1fd361209fe9661
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-23 02:38:45 -0300

    feat: create login route

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const investmentsRoute = require('./routes/investmentsRoute');
 const accountRoute = require('./routes/accountRoute');
 const errorMiddleware = require('./middlewares/error');
 const assetsRoute = require('./routes/assetsRoute');
+const loginRoute = require('./routes/loginRoute');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/', investmentsRoute);
 app.use('/', accountRoute);
 app.use('/', assetsRoute);
+app.use('/', loginRoute);
 app.use(errorMiddleware);
 
 module.exports = app;
