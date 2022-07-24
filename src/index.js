1ddd1738876df0c29fa62c9b39bd856cf05727b2
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-24 11:04:00 -0300

    refactor:remove code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-
 const PORT = process.env.PORT;
-
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript')
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
