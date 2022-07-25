7552816a7acd03b570026134e49565c13ef38287
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-24 23:19:41 -0300

    feat:swagger of account

--- src/routes/accountRoute.js
+++ src/routes/accountRoute.js
@@ -11,6 +11,36 @@ const depositMiddle = require('../middlewares/depositMiddleware');
  *    description: Endpoints de Accounts
  */
 
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *       Account:
+ *           type: object
+ *           required:
+ *            - codCliente
+ *           properties:
+ *              codClinete:
+ *                  type: string
+ *              saldo:
+ *                  type: number
+ */
+// /**
+//  * @swagger
+//  *   /account/:codCliente:
+//  *     get:
+//  *       tags:
+//  *         [Account]
+//  *       description: Endpoint retorna a conta do cliente
+//  *       responses:
+//  *        200:
+//  *         content:
+//  *           appliccation/json:
+//  *            schema:
+//  *              type: array
+//  *              items:
+//  *                $ref: '#/components/schemas/Account'
+//  */
 router.get('/account/:codCliente', controller.getCodCliente);
 router.post('/account/withdraw', withdrawMiddle, controller.addWithdraw);
 router.post('/account/deposit', depositMiddle, controller.addDeposit);
