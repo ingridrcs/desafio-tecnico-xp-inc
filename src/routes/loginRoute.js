14bce2d6c9c8b52ad51ae85362390510811bec11
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-24 22:49:33 -0300

    fix:commentend code

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -3,50 +3,50 @@ const router = express.Router();
 const loginUser = require('../controllers/loginController');
 const loginMiddle = require('../middlewares/loginMiddleware');
 
-/**
- * @swagger
- *  tags:
- *    name: User
- *    description: Endpoints de User
- */
+// /**
+//  * @swagger
+//  *  tags:
+//  *    name: User
+//  *    description: Endpoints de User
+//  */
 
-/**
- * @swagger
- *  components:
- *    schemas:
- *       User:
- *           type: object
- *           required:
- *            - email
- *            - password
- *           properties:
- *              id:
- *                  type: string
- *              email:
- *                  type: string
- *              password:
- *                  type: string
- *           examples:
- *              email: ingrid@gmail.com
- *              password: 123456
- */
+// /**
+//  * @swagger
+//  *  components:
+//  *    schemas:
+//  *       User:
+//  *           type: object
+//  *           required:
+//  *            - email
+//  *            - password
+//  *           properties:
+//  *              token:
+//  *                  type: string
+//  *              email:
+//  *                  type: string
+//  *              password:
+//  *                  type: string
+//  *           examples:
+//  *              email: ingrid@gmail.com
+//  *              password: 123456
+//  */
 
-/**
- * @swagger
- *   /login:
- *     get:
- *       tags:
- *         [User]
- *       description: Endpoint retorna uma lista de usuários
- *       responses:
- *        200:
- *         content:
- *           appliccation/json:
- *            schema:
- *              type: array
- *              items:
- *                $ref: '#/components/schemas/User'
- */
+// /**
+//  * @swagger
+//  *   /login:
+//  *     get:
+//  *       tags:
+//  *         [User]
+//  *       description: Endpoint retorna uma lista de usuários
+//  *       responses:
+//  *        200:
+//  *         content:
+//  *           appliccation/json:
+//  *            schema:
+//  *              type: array
+//  *              items:
+//  *                $ref: '#/components/schemas/User'
+//  */
 router.post('/login', loginMiddle, loginUser);
 
 module.exports = router;
